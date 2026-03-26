2fd913fd402071c89e57aec1007bec7af5f8a007
Tiago <tiaguinhoo.cruz@hotmail.com>
2026-03-26 17:09:44 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+function App() {
+  const [count,setCount] = useState(0)
+ 
+
+  return (
+    <>
+     <p>Hello world</p>
+     <button onClick={()=>
+      setCount(count-1)
+     }>
+      Count {count}
+     </button>
+    </>
+  )
+}
+
+export default App
